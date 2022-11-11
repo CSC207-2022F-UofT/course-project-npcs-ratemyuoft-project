310b2d561bd930f34d4365dd57f14ee8539f5f49
Mykolae <mykola.zhuk@mail.utoronto.ca>
2022-11-10 23:17:33 -0400

    Made all the entities

---
+++ src/main/entities/Admin.java
@@ -0,0 +1,9 @@
+package entities;
+public class Admin extends User{
+
+    public boolean isCool = true;
+
+    public Admin(String username, String password){
+        super(username, password);
+    }
+}
