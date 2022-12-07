f4d92073d339e4ea09ea845bd1d8da9cb4922720
KenanBogopa <113219092+KenanBogopa@users.noreply.github.com>
2022-12-06 23:50:55 -0500

    I have added docstrings to the course class.

--- src/main/java/entities/Course.java
+++ src/main/java/entities/Course.java
@@ -5,30 +5,40 @@ import java.util.ArrayList;
 
 /**
  * Course is the core entity in our RateMyUofT system application.
- * <p>
- *     Course is the structure that holds the application. It stores course information. And it contains its reviews.
- * </p>
- *
+ * Course is the structure that holds the application. It stores course information. And it contains its reviews.
  */
 public class Course implements Serializable {
+
     /**
-     * Course has four instance variables.
-     * <p>
-     * courseName: String
-     * <p>
-     * reviews: Arraylist<Reviews>
-     * <p>
-     * courseRating: double
-     * <p>
-     * fieldOfStudy: String
+     * This is the name of the course.
      */
     private String courseName;
+
+    /**
+     * This is the list of reviews that this course currently has.
+     */
     private final ArrayList<Review> reviews= new ArrayList<>();
+
+    /**
+     * This the rating that this course has which is based on how good or bad its reviews are.
+     */
     private double courseRating = 0.0;
+
+    /**
+     * This is the academic area that this course is on (Field of Study).
+     */
     private String fieldOfStudy;
 
+    /**
+     * This is the total ratings that this course has form its reveiws. This is used to calculate the course's
+     * (average) rating.
+     */
     private int ratingCount;
 
+    /**
+     * This is the number of reviews that this course has. This is used to calculate the course's (average) rating
+     * and to assign unique ReviewIDs to the reviews of this course.
+     */
     private int reviewCount = 0;
 
     /**
@@ -44,28 +54,68 @@ public class Course implements Serializable {
     }
 
     //Getters
+
+    /**
+     * This getter returns the name of the course.
+     */
     public String getCourseName() {
         return this.courseName;
     }
+
+    /**
+     * This getter returns the field of study that this course is in.
+     */
     public String getFieldOfStudy() { return this.fieldOfStudy; }
+
+    /**
+     * This getter returns the rating of this course.
+     */
     public double getCourseRating() { return this.courseRating; }
+
+    /**
+     * This getter returns the list of reviews that this course has.
+     */
     public ArrayList<Review> getReviews() { return reviews;}
+
+    /**
+     * This getter returns the total number of reviews that this course has.
+     */
     public int getReviewCount() {
         return reviewCount;
     }
 
+    /**
+     * This getter returns the total of ratings from all of this course's reviews.
+     */
     public int getRatingCount() {return ratingCount;}
 
     //Setters
+
+    /**
+     * This setter sets the name of the course to what is the parameter.
+     */
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
+
+    /**
+     * This setter sets the rating of the course to what is the parameter.
+     */
     public void setCourseRating(double courseRating) {
         this.courseRating = courseRating;
     }
+
+    /**
+     * This setter sets the field of study of the course to what is the parameter.
+     */
     public void setFieldOfStudy(String fieldOfStudy) {
         this.fieldOfStudy = fieldOfStudy;
     }
+
+    /**
+     * This method adds a review object to the course and also updates the reviewCount, rating count and course rating
+     * of the course.
+     */
     public void addReview(Review r) {
         ratingCount+= r.getRating();
         reviewCount += 1;
